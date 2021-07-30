c0889e403a592cf0e0597abb96d6fa63b65b22b6
kaiquebellmont <kaiquebellmont@gmail.com.br>
2021-07-30 10:17:32 -0300

    put and post

--- JS/apiExpressNodeJS/server.js
+++ JS/apiExpressNodeJS/server.js
@@ -1,14 +1,16 @@
 const express = require('express');
 const app = express();
 const chalk = require('chalk');
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 
-const {getUsers, getUserById, putUserById} = require('./src/teste');
+const {getUsers, getUserById, putUserById, createUser} = require('./src/teste');
 
-app.get('/', (req, res)=>{
+app.get('/', (req, res) => {
     res.send('Olá, este é o endereço raiz');
 });
 
-app.get('/api/v1/users', async (req, res) =>{
+app.get('/api/v1/users', async (req, res) => {
     res.json(await getUsers());
 })
 
@@ -16,12 +18,14 @@ app.get('/api/v1/users/:id', async (req, res) => {
     res.json(await getUserById(req.params.id));
 });
 
-app.put('/api/v1/users/:id', async(req, res) => {
-    res.json("Isso é um put!");
-    res.json(await putUserById(req.params.id));
-    
+app.put('/api/v1/users/:id', async (req, res) => {
+    res.json(await putUserById(req.params.id, req.body))
 })
 
-app.listen(3000, ()=>{
-    console.log('Listening on port '+ chalk.green('3000'));
+app.post('/api/v1/users', async (req, res) => {
+    res.json(await createUser(req.body));
+});
+
+app.listen(4010, () => {
+    console.log('Listening on port ' + chalk.green('4010'));
 });
--- JS/apiExpressNodeJS/src/teste.js
+++ JS/apiExpressNodeJS/src/teste.js
@@ -1,31 +1,53 @@
-const { request } = require('express');
+const {request} = require('express');
 const data = require('../mock_data.json');
+const fs = require("fs");
 
-async function getUsers(){
-  return new Promise((resolve, reject)=>{
-    resolve(data);
-  })
-} 
 
-async function getUserById(id_user){
-  return new Promise((resolve, reject)=>{
-    for (const item of data) {
-      if(item['id'] == id_user){
-          resolve(item);
-      };
-    };
-  })
+async function getUsers() {
+    return new Promise((resolve, reject) => {
+        resolve(data);
+    })
 }
 
-async function putUserById(id_user){
-  return new Promise((resolve, reject)=>{
-    for (const item of data) {
-      if(item['id'] == id_user){
-          resolve(item);
-      };
-    };
-  })
-} 
+async function createUser(userData) {
+    return new Promise((resolve, reject) => {
+        userData.id = data.length + 1 + ""
+        data.push(userData)
+        resolve(userData);
+        fs.writeFileSync('mock_data.json', JSON.stringify(data, null, "\t"));
+    })
+}
 
-module.exports = {getUsers, getUserById, putUserById}
+async function getUserById(id_user) {
+    return new Promise((resolve, reject) => {
+        for (const item of data) {
+            if (item['id'] === id_user) {
+                resolve(item);
+            }
+        }
+    })
+}
+
+async function putUserById(id_user, userData) {
+    return new Promise((resolve, reject) => {
+        let updatedUserData;
+        for (const item of data) {
+            if (item['id'] === id_user) {
+                item.first_name = userData.first_name ? userData.first_name : item.first_name
+                item.last_name = userData.last_name ? userData.last_name : item.last_name
+                item.email = userData.email ? userData.email : item.email
+                item.gender = userData.gender ? userData.gender : item.gender
+                item.ip_address = userData.ip_address ? userData.ip_address : item.ip_address
+                console.log(item)
+                updatedUserData = item
+                break
+            }
+        }
+
+        fs.writeFileSync('mock_data.json', JSON.stringify(data, null, "\t"));
+        resolve(updatedUserData)
+    })
+}
+
+module.exports = {getUsers, getUserById, putUserById, createUser}
 // criar uma outra função getuser
